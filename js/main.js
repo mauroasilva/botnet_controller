6e24eabd780e31284158dd0f8c7e0b9225d86d35
Mauro <life@phanes.corp.fccn.pt>
2014-08-06 17:05:49 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,9 +2,82 @@ var nodes = null;
 var edges = null;
 var graph = null;
 
-window.onresize = function(event) {
-    var container = document.getElementById('mygraph');
-    container.style.height = window.innerHeight * 0.8 + "px";
+var CORE_FIELDS = 3;
+
+var popup = document.getElementById("graph-popUp");
+var span = document.getElementById('graph-popUp-title');
+var table = document.getElementById("graph-popUp-fields");
+
+function disable_file_submit() {
+    var file_form = document.getElementById("file-select");
+    var graph_row = document.getElementById("graph-row");
+    file_form.style.display = 'none';
+    graph_row.style.display = 'block';
+}
+
+function load_file(elem_id, callback) {
+    var file = document.getElementById(elem_id).files[0];
+    var file_result = undefined;
+    
+    var reader = new FileReader();
+    reader.onload = (function (event) {
+        obj = JSON.parse(event.target.result);
+        callback(obj);
+    });
+    reader.readAsText(file);
+}
+
+function verify_files() {
+    if (document.getElementById("bots-file").files[0] && 
+        document.getElementById("pipeline-file").files[0] && 
+        document.getElementById("runtime-file").files[0]) {
+        load_file('bots-file', load_bots);
+    } else {
+        alert('There are some files missing');
+    }
+    var body = document.getElementsByTagName('body')[0];
+    var container = document.getElementsByClassName('container-fluid')[0];
+    body.style.height = window.innerHeight * 0.9 + "px";
+    container.style.height = window.innerHeight * 0.9 + "px";
+}
+
+function load_bots(config) {
+    // Do stuff
+    // Bots need: label, id, group which corresponds to: label = id = bot_name, group = [collector, parser, expert, output]
+    for(bot_group in config) {
+        var group = config[bot_group];
+        
+        available_bots = document.getElementById("available-bots")
+        group_title = document.createElement('h5');
+        group_title.innerHTML = "&nbsp;&nbsp;" + bot_group;
+        available_bots.appendChild(group_title);
+        
+        for (bot_name in group) {
+            var bot = group[bot_name];
+            bot_title = document.createElement('h6');
+            bot_title.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;" + bot_name;
+            available_bots.appendChild(bot_title);
+            
+            for (parameter in bot['parameters']) {
+                var value = bot['parameters'][parameter];
+            }
+        }
+    }
+    
+    load_file('runtime-file', load_runtime);
+}
+
+function load_runtime(config) {
+    // Do stuff
+        
+    load_file('pipeline-file', load_pipeline);
+}
+
+function load_pipeline(config) {
+    // Do stuff
+        
+    disable_file_submit();
+    draw();
 }
 
 function draw() {
@@ -12,7 +85,6 @@ function draw() {
 
     // create a graph
     var container = document.getElementById('mygraph');
-    container.style.height = window.innerHeight * 0.8 + "px";
 
     var data = {
         nodes: [],
@@ -33,17 +105,51 @@ function draw() {
 	alert("ERROR LOADING DATA");
         continue_drawing(connectionCount, container, data); 
     });
+    
+    popup = document.getElementById("graph-popUp");
+    span = document.getElementById('graph-popUp-title');
+    table = document.getElementById("graph-popUp-fields");
 }
 
 function create_form(title){
-    var popup = document.getElementById("graph-popUp");
-    var span = document.getElementById('graph-popUp-title');
-    
     span.innerHTML = title;
 }
 
+function load_form(data){
+    for (key in data.custom_fields) {
+        new_row = table.insertRow(-1);
+        cell1 = new_row.insertCell(0);
+        cell2 = new_row.insertCell(1);
+        cell3 = new_row.insertCell(2);
+        
+        cell1.innerHTML = key
+        cell2_content = document.createElement("input");
+        cell2_content.setAttribute('type', 'text');
+        cell2_content.setAttribute('value', data.custom_fields[key]);
+        cell2.appendChild(cell2_content);
+        
+        cell3_content = document.createElement("input");
+        cell3_content.setAttribute('type', 'button');
+        cell3_content.setAttribute('value', 'delete');
+        cell3_content.setAttribute('onclick', 'delete_field(this);');
+        cell3.appendChild(cell3_content);
+    }
+}
+
+function delete_form(){
+    
+    span.innerHTML = "";    
+    
+    for (i = table.rows.length-1; i >= 0; i--) { 
+        var position = table.rows[i].rowIndex;
+        
+        if (position >= CORE_FIELDS) {
+            table.deleteRow(position);
+        }
+    }
+}
+
 function add_field() {
-    var table = document.getElementById("graph-popUp-fields");
     new_row = table.insertRow(-1);
     cell1 = new_row.insertCell(0);
     cell2 = new_row.insertCell(1);
@@ -53,6 +159,10 @@ function add_field() {
     
     cell1.appendChild(cell1_content);
     cell2.appendChild(cell2_content);
+}
+
+function delete_field(row) {
+    table.deleteRow(row.parentElement.parentElement.rowIndex);
 }
 
 function continue_drawing(connectionCount, container, data) {
@@ -65,23 +175,22 @@ function continue_drawing(connectionCount, container, data) {
         },
         groups: {
             input: {
-                shape: 'dot',
-                color: "#2B7CE9" // blue
+                shape: 'circle',
+                color: "#33CC33" // blue
             },
             output: {
-                shape: 'dot',
-                color: "#5A1E5C" // purple
+                shape: 'database',
+                color: "#33CC33" // purple
             },
             expert: {
-                shape: 'dot',
-                color: "#C5000B" // red
+                shape: 'box',
+                color: "#33CC33" // red
             }
         },
         stabilize: false,
         dataManipulation: true,
         navigation: true,
         onAdd: function(data,callback) {
-            var span = document.getElementById('graph-popUp-title');
             var idInput = document.getElementById('node-id');
             var labelInput = document.getElementById('node-label');
             var groupInput = document.getElementById('node-group');
@@ -99,7 +208,6 @@ function continue_drawing(connectionCount, container, data) {
             div.style.display = 'block';
         },
         onEdit: function(data,callback) {
-            var span = document.getElementById('graph-popUp-title');
             var idInput = document.getElementById('node-id');
             var labelInput = document.getElementById('node-label');
             var groupInput = document.getElementById('node-group');
@@ -108,6 +216,7 @@ function continue_drawing(connectionCount, container, data) {
             var addFieldButton = document.getElementById('graph-popUp-add');
             var div = document.getElementById('graph-popUp');
             create_form("Edit Node");
+            load_form({'custom_fields': {'cenas': 'asd', 'cenas2': 'asd2'}});
             idInput.value = data.id;
             labelInput.value = data.label;
             groupInput.value = data.group;
@@ -155,6 +264,7 @@ function continue_drawing(connectionCount, container, data) {
         cancelButton.onclick = null;
         var div = document.getElementById('graph-popUp');
         div.style.display = 'none';
+        delete_form();
     }
 
     function saveData(data,callback) {
